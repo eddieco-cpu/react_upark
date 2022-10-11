866474f2c8f6d541fe4496e5bbf9561498f552a1
Eddie Ko <linyi.ko@udngroup.com>
2022-10-11 17:06:20 +0800

    [try] fix proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,25 +1,31 @@
-const proxy = require('http-proxy-middleware')
+const proxy = require("http-proxy-middleware");
 
-module.exports = function(app){
-	app.use(
+module.exports = function (app) {
+  app.use(
+    //筆記
+    proxy("/api1", {
+      //遇见/api1前缀的请求，就会触发该代理配置
+      target: "http://localhost:5000", //请求转发给谁
+      changeOrigin: true, //控制服务器收到的请求头中Host的值
+      pathRewrite: { "^/api1": "" }, //重写请求路径(必须)
+    }),
 
-		//筆記
-		proxy('/api1', { 	//遇见/api1前缀的请求，就会触发该代理配置
-			target:'http://localhost:5000',		//请求转发给谁
-			changeOrigin:true,		//控制服务器收到的请求头中Host的值
-			pathRewrite:{'^/api1':''} 		//重写请求路径(必须)
-		}),
+    proxy("/govapi", {
+      target: "https://od.moi.gov.tw",
+      changeOrigin: true,
+      pathRewrite: { "^/govapi": "" },
+    }),
 
-		proxy('/govapi',{
-			target:'https://od.moi.gov.tw',
-			changeOrigin: true,
-			pathRewrite:{'^/govapi':''}
-		}),
+    proxy("/ntpcapi", {
+      target: "https://data.ntpc.gov.tw",
+      changeOrigin: true,
+      pathRewrite: { "^/ntpcapi": "" },
+    })
 
-		proxy('/ntpcapi',{
-			target:'https://data.ntpc.gov.tw',
-			changeOrigin: true,
-			pathRewrite:{'^/ntpcapi':''}
-		}),
-	)
-}
+    // proxy("/build", {
+    //   target: "https://data.ntpc.gov.tw",
+    //   changeOrigin: true,
+    //   pathRewrite: { "^/build": "" },
+    // })
+  );
+};
